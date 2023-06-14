77b26e1f16fd1a72cb5b46ebf3a4bb06fc2f463c
soladen2010 <soladen2010@gmail.com>
2023-06-14 09:45:56 +0800

    0614 09:45

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -23,3 +23,47 @@ func TestForEachParal(t *testing.T) {
 		fmt.Println(e.id, e.name)
 	})
 }
+
+func TestMap(t *testing.T) {
+	arr := []User{{id: 1, name: "tom"}, {id: 2, name: "jack"}}
+	intArr := Map(arr, func(e User) int {
+		return e.id
+	})
+	fmt.Println(intArr)
+}
+
+func TestMapParal(t *testing.T) {
+	arr := []User{{id: 1, name: "tom"}, {id: 2, name: "jack"}, {id: 3, name: "marry"}}
+	intArr := MapParal(arr, 2, func(e User) int {
+		return e.id
+	})
+	fmt.Println(intArr)
+}
+
+func TestReduce(t *testing.T) {
+	arr := []User{{id: 1, name: "tom"}, {id: 2, name: "jack"}, {id: 3, name: "marry"}}
+	sum := Reduce(arr, 0, func(s int, e User) int {
+		return s + e.id
+	})
+	fmt.Println(sum)
+}
+
+func TestFilter(t *testing.T) {
+	arr := []User{{id: 1, name: "tom"}, {id: 2, name: "jack"}, {id: 3, name: "marry"}}
+	filteredArr := Filter(arr, func(e User) bool {
+		return e.id > 1
+	})
+	fmt.Println(filteredArr)
+}
+
+func TestReverse(t *testing.T) {
+	arr := []User{{id: 1, name: "tom"}, {id: 2, name: "jack"}, {id: 3, name: "marry"}}
+	reversedArr := Reverse(arr)
+	fmt.Println(reversedArr)
+}
+
+func TestGroupCount(t *testing.T) {
+	arr := []User{{id: 1, name: "tom"}, {id: 2, name: "jack"}, {id: 3, name: "marry"}, {id: 2, name: "jack"}}
+	countMap := GroupCount(arr)
+	fmt.Println(countMap)
+}
